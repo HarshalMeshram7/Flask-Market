e81ca0b92deedc342d7fc2fb9b5be49885b982d8
Harshal Meshram <harshalmeshram070@gmail.com>
2025-06-11 23:57:52 +0530

    template inheritance completed

--- market.py
+++ market.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route('/home')
-def hello_world():
+def home_page():
     return render_template('home.html')
 
 @app.route('/market')
