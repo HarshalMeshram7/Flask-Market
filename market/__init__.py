aa096fa0007609174f285e273d41cacff72ab6bd
Harshal Meshram <harshalmeshram112@gmail.com>
2025-06-12 16:35:48 +0530

    register page added

--- market/__init__.py
+++ market/__init__.py
@@ -8,6 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'market.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'fee6fe778ae479d162c8c104'
 db = SQLAlchemy(app)
 
 from market import routes   
