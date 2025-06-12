79a360822e6ddbaca3301c2aa50cbbcaed47bfdc
Harshal Meshram <harshalmeshram112@gmail.com>
2025-06-12 17:04:37 +0530

    register page validations completed

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegiterForm
+
 
 @app.route("/")
 @app.route('/home')
@@ -13,7 +14,17 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegiterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #If there are not errors in the errors dictionary
+        for err_msg in form.errors.values():
+            print(f'There was an error with the form: {err_msg}')
     return render_template('register.html', form=form)
